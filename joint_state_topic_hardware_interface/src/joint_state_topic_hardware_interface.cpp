7e6b78dedcb6b6e70b1cd324555f2a9eedfa39e0
marqrazz <marq.razz@gmail.com>
2025-07-25 09:29:39 -0600

    Switch to on_init(HardwareComponentInterfaceParams)

--- joint_state_topic_hardware_interface/src/joint_state_topic_hardware_interface.cpp
+++ joint_state_topic_hardware_interface/src/joint_state_topic_hardware_interface.cpp
@@ -47,9 +47,9 @@ static constexpr std::size_t VELOCITY_INTERFACE_INDEX = 1;
 // JointState doesn't contain an acceleration field, so right now it's not used
 static constexpr std::size_t EFFORT_INTERFACE_INDEX = 3;
 
-CallbackReturn JointStateTopicSystem::on_init(const hardware_interface::HardwareInfo& info)
+CallbackReturn JointStateTopicSystem::on_init(const hardware_interface::HardwareComponentInterfaceParams& params)
 {
-  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  if (hardware_interface::SystemInterface::on_init(params) != CallbackReturn::SUCCESS)
   {
     return CallbackReturn::ERROR;
   }
