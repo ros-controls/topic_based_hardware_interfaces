ba671683d232caeb45f0228a6ff366aa3e34c1fd
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-09-24 09:23:33 +0000

    Update copyright claim

--- cm_topic_hardware_component/include/cm_topic_hardware_component/cm_topic_hardware_component.hpp
+++ cm_topic_hardware_component/include/cm_topic_hardware_component/cm_topic_hardware_component.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 ros2_control Development Team
+// Copyright 2025 AIT Austrian Institute of Technology GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* Author: Jafar Abdi */
+/* Author: Christoph Froehlich */
 
 #pragma once
 
--- cm_topic_hardware_component/test/cm_topic_hardware_component_test.cpp
+++ cm_topic_hardware_component/test/cm_topic_hardware_component_test.cpp
@@ -1,4 +1,4 @@
-// Copyright 2025 ros2_control Development Team
+// Copyright 2025 AIT Austrian Institute of Technology GmbH
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+/* Author: Christoph Froehlich */
 
 #include <cmath>
 #include <string>
