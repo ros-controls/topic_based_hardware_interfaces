0700746a788cbc8318aa581f822b0f443278812a
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2025-10-13 20:59:44 +0000

    Wrap another Path obj with str()

--- joint_state_topic_hardware_interface/test/control.launch.py
+++ joint_state_topic_hardware_interface/test/control.launch.py
@@ -23,7 +23,7 @@ SCRIPT_PATH = Path(os.path.realpath(__file__)).parent
 
 
 def generate_launch_description():
-    ros2_controllers_file = Path(SCRIPT_PATH / "ros2_controllers.yaml")
+    ros2_controllers_file = str(Path(SCRIPT_PATH / "ros2_controllers.yaml"))
     robot_description = {
         "robot_description": xacro.process_file(SCRIPT_PATH / "rrr.urdf.xacro").toxml(),
     }
